47eabb640ff7039df4cb359aef0daeedf7c6ac56
Clement Michaud <c.michaud@criteo.com>
2018-03-09 11:49:18 +0100

    Fix patch_version.sh to remove v from tag when building Marathon
    Without this fix, Marathon does not detect the version correctly (its
    expects to have no v in the version you can find in META-INF/MANIFEST.MF
    in marathon jar).
    
    This triggers a migration of app that is failing...

--- patch_version.sh
+++ patch_version.sh
@@ -3,5 +3,6 @@
 # this script will patch version.sbt to put the right version if we are building a tag
 
 if [[ ! -z "$TRAVIS_TAG" ]]; then
-  echo "version in ThisBuild := \"$TRAVIS_TAG\"" > version.sbt
+  VERSION=$(echo $TRAVIS_TAG | sed 's/^v//')
+  echo "version in ThisBuild := \"$VERSION\"" > version.sbt
 fi
