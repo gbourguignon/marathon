931456590dc45cbdf1c50fa1ced94fae69330706
Grégoire Seux <g.seux@criteo.com>
2018-09-11 17:56:22 +0200

    Make sure networkBandwidth can be specified in all scenarii
    Before this patch:
    - updates with `partialUpdate: true` allow networkBandwidth to be
    changed
    - updates with `partialUpdate: false` (or without any mention of it)
      ignore the networkBandwidth field
    - app creation properly treated networkBandwidth field
    
    After this patch, all of those scenarios works correctly.
    
    I'm not yet able to write a test for the faulty scenario. It may come
    later
    
    Change-Id: I2120a82a03bacd581836ed116911aa3f17ec2f41

--- src/main/scala/mesosphere/marathon/state/AppDefinition.scala
+++ src/main/scala/mesosphere/marathon/state/AppDefinition.scala
@@ -150,6 +150,7 @@ case class AppDefinition(
     val memResource = ScalarResource(Resource.MEM, resources.mem)
     val diskResource = ScalarResource(Resource.DISK, resources.disk)
     val gpusResource = ScalarResource(Resource.GPUS, resources.gpus.toDouble)
+    val networkBandwidthResource = ScalarResource(Resource.NETWORK_BANDWIDTH, resources.networkBandwidth.toDouble)
     val appLabels = labels.map {
       case (key, value) =>
         mesos.Parameter.newBuilder
@@ -173,6 +174,7 @@ case class AppDefinition(
       .addResources(memResource)
       .addResources(diskResource)
       .addResources(gpusResource)
+      .addResources(networkBandwidthResource)
       .addAllHealthChecks(healthChecks.map(_.toProto).asJava)
       .setUpgradeStrategy(upgradeStrategy.toProto)
       .addAllDependencies(dependencies.map(_.toString).asJava)
@@ -293,7 +295,8 @@ case class AppDefinition(
         cpus = resourcesMap.getOrElse(Resource.CPUS, this.resources.cpus),
         mem = resourcesMap.getOrElse(Resource.MEM, this.resources.mem),
         disk = resourcesMap.getOrElse(Resource.DISK, this.resources.disk),
-        gpus = resourcesMap.getOrElse(Resource.GPUS, this.resources.gpus.toDouble).toInt
+        gpus = resourcesMap.getOrElse(Resource.GPUS, this.resources.gpus.toDouble).toInt,
+        networkBandwidth = resourcesMap.getOrElse(Resource.NETWORK_BANDWIDTH, this.resources.networkBandwidth.toDouble).toInt
       ),
       env = envMap ++ envRefs,
       fetch = proto.getCmd.getUrisList.map(FetchUri.fromProto)(collection.breakOut),
@@ -619,8 +622,17 @@ object AppDefinition extends GeneralPurposeCombinators {
       appDefinition.resources.gpus as "gpus" is isTrue(PersistentVolumeResourcesChanged) { gpus => from.resources.gpus == gpus }
       appDefinition.requirePorts is isTrue(PersistentVolumeResourcesChanged) { requirePorts => from.requirePorts == requirePorts }
       appDefinition is isTrue(PersistentVolumeHostPortsChanged) { to =>
-        from.hostPorts.flatten.toSet == to.hostPorts.flatten.toSet
+        from.resources.networkBandwidth == to.resources.networkBandwidth &&from.hostPorts.flatten.toSet == to.hostPorts.flatten.toSet
       }
+      /*
+            from.resources.cpus == to.resources.cpus &&
+          from.resources.mem == to.resources.mem &&
+          from.resources.disk == to.resources.disk &&
+          from.resources.gpus == to.resources.gpus &&
+          from.resources.networkBandwidth == to.resources.networkBandwidth &&
+          from.hostPorts.flatten.toSet == to.hostPorts.flatten.toSet &&
+          from.requirePorts == to.requirePorts
+       */
     }
 
     validator[AppDefinition] { app =>
