dc0bc650d884f1f945f9f5c488bd88d719455be5
Grégoire Seux <g.seux@criteo.com>
2019-10-23 08:49:26 +0200

    Display current branch when not matching criteo pattern
    Change-Id: I0337ef868a03ee07bec4d5f8b76178d108c4d3cc

--- criteo_tag_commit.sh
+++ criteo_tag_commit.sh
@@ -6,19 +6,19 @@ echo $CURRENT_BRANCH
 
 generate_criteo_patch()
 {
-    CRITEO_VERSION=$(git describe --tags --match="v$BASE_VERSION-criteo*" 2>/dev/null)
-    echo "last criteo patch was described as $CRITEO_VERSION"
-    if [[ -z $CRITEO_VERSION ]]
-    then
-        CRITEO_INCREMENT=1
-    elif [[ $CRITEO_VERSION =~ criteo[0-9]+-[0-9]+ ]]
-    then
-        CURRENT_INCREMENT=$(echo $CRITEO_VERSION | sed 's/^.*criteo\([0-9]\+\)-.*$/\1/')
-        CRITEO_INCREMENT=$((CURRENT_INCREMENT + 1))
-    else
-        echo "commit already tagged"
-        exit
-    fi
+  CRITEO_VERSION=$(git describe --tags --match="v$BASE_VERSION-criteo*" 2>/dev/null)
+  echo "last criteo patch was described as $CRITEO_VERSION"
+  if [[ -z $CRITEO_VERSION ]]
+  then
+    CRITEO_INCREMENT=1
+  elif [[ $CRITEO_VERSION =~ criteo[0-9]+-[0-9]+ ]]
+  then
+    CURRENT_INCREMENT=$(echo $CRITEO_VERSION | sed 's/^.*criteo\([0-9]\+\)-.*$/\1/')
+    CRITEO_INCREMENT=$((CURRENT_INCREMENT + 1))
+  else
+    echo "commit already tagged"
+    exit
+  fi
 }
 
 function updateVersion() {
@@ -29,17 +29,17 @@ function updateVersion() {
 
 if [[ $CURRENT_BRANCH =~ ^criteo\/ ]]
 then
-    BASE_VERSION=${CURRENT_BRANCH#*/}
-    echo "criteo branch forked from base version: $BASE_VERSION"
-    generate_criteo_patch
-    CRITEO_PATCH="criteo${CRITEO_INCREMENT:-1}"
-    echo "new criteo patch: $CRITEO_PATCH"
-    CRITEO_TAG="v$BASE_VERSION-$CRITEO_PATCH"
-    echo "tagging current commit with $CRITEO_TAG"
-    git tag $CRITEO_TAG
-    git remote add github https://${GH_TOKEN}@github.com/criteo-forks/marathon > /dev/null 2>&1
-    git push --quiet github --tags
-    updateVersion $CRITEO_TAG
+  BASE_VERSION=${CURRENT_BRANCH#*/}
+  echo "criteo branch forked from base version: $BASE_VERSION"
+  generate_criteo_patch
+  CRITEO_PATCH="criteo${CRITEO_INCREMENT:-1}"
+  echo "new criteo patch: $CRITEO_PATCH"
+  CRITEO_TAG="v$BASE_VERSION-$CRITEO_PATCH"
+  echo "tagging current commit with $CRITEO_TAG"
+  git tag $CRITEO_TAG
+  git remote add github https://${GH_TOKEN}@github.com/criteo-forks/marathon > /dev/null 2>&1
+  git push --quiet github --tags
+  updateVersion $CRITEO_TAG
 else
-    echo current branch is not a criteo branch
+  echo "current branch (${CURRENT_BRANCH}) is not a criteo branch"
 fi
