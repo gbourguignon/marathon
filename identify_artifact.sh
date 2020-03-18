6831f52516666cba3fb03f00c964143f9480caf7
Julien Pepy <j.pepy@criteo.com>
2020-03-18 21:55:53 +0100

    debug2

--- identify_artifact.sh
+++ identify_artifact.sh
@@ -4,6 +4,9 @@ artifact_path=$(ls -1 target/universal/marathon-*.tgz)
 artifact=$(basename "$artifact_path")
 artifact_version="$(echo $artifact | sed -E 's/marathon-(.*)\.tgz/\1/')"
 
+git describe
+git describe --tags
+./version tag
 ls -l target/universal
 echo "::set-output name=path::$artifact_path"
 echo "::set-output name=artifact::$artifact"
